85624f048f098c3e942a407000412e849cf93350
ssarber <ssarber@gmail.com>
2016-09-01 13:53:57 -0700

    Initial Commit

---
+++ ShotsFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShotsFeed
+//
+//  Created by Zook Gek on 9/1/16.
+//  Copyright © 2016 Stan Sarber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
